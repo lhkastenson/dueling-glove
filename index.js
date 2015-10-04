6b8d3382d457c259f241d5126f53beab9629dbbb
lhkastenson <lhkastenson@gmail.com>
2015-10-04 00:37:20 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var request = require('request');
+
+var Exception = require('./exception.js');
+
+module.exports = challonge;
+
+//require('util').inherits(challonge, require('events').EventsEmitter);
